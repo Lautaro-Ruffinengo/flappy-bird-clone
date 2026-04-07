0f2d71e348fbbe96f255cbc96eb2badff188a721
Lautaro <lauruffinengo@gmail.com>
2026-04-07 18:00:40 -0300

    Implement jump mechanic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,16 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private InputActionReference jumpAction;
 
+    [SerializeField] private float jumpForce;
+    
+
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
     private void OnEnable()
     {
         jumpAction.action.performed += Jump;
@@ -19,6 +29,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Jump(InputAction.CallbackContext context)
     {
+        _rb.linearVelocityY = jumpForce;
         Debug.Log("Jumping!");
     }
 }
